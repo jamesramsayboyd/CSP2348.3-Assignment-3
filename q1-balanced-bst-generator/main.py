4b70068aaec39f1c33a16f7a93b42eb92b2cbb0c
James Boyd <88956638+jamesramsayboyd@users.noreply.github.com>
2023-04-21 21:35:07 +0800

    More experimenting with BST printing methods

--- q1-balanced-bst-generator/main.py
+++ q1-balanced-bst-generator/main.py
@@ -1,149 +1,205 @@
-# Source: CSP2348_M6_Binary Trees.pptx
-class TreeNode:
-    def __init__(self, e):
-        self.element = e
+# # Source: CSP2348_M6_Binary Trees.pptx
+# class TreeNode:
+#     def __init__(self, e):
+#         self.element = e
+#         self.left = None
+#         self.right = None
+#
+#
+# # Source: CSP2348_M6_Binary Trees.pptx
+# class BinaryTree:
+#     def __init__(self):
+#         self.root = None
+#         self.size = 0
+#
+#
+# # https://www.geeksforgeeks.org/binary-search-tree-set-1-search-and-insertion/
+# def insert_into_bst(root, e):
+#     if root is None:
+#         return TreeNode(e)
+#     else:
+#         if root.element is e:
+#             return root
+#         elif root.element < e:
+#             root.right = insert_into_bst(root.right, e)
+#         else:
+#             root.left = insert_into_bst(root.left, e)
+#     return root
+#
+#
+# # https://www.geeksforgeeks.org/binary-search-tree-set-1-search-and-insertion/
+# def inorder(root):
+#     if root:
+#         inorder(root.left)
+#         print(root.element)
+#         inorder(root.right)
+#
+#
+# def print_space(n, removed):
+#     for i in range(n):
+#         print("\t", end="")
+#     if removed is None:
+#         print(" ", end="")
+#     else:
+#         print(removed.element, end="")
+#
+#
+# def height_of_tree(root):
+#     if root is None:
+#         return 0
+#     return 1 + max(height_of_tree(root.left), height_of_tree(root.right))
+#
+#
+# def print_binary_tree(root):
+#     tree_level = []
+#     temp = []
+#     tree_level.append(root)
+#     counter = 0
+#     height = height_of_tree(root) - 1
+#     number_of_elements = 2 ** (height + 1) - 1
+#     while counter <= height:
+#         removed = tree_level.pop(0)
+#         if len(temp) == 0:
+#             print_space(int(number_of_elements / (2 ** (counter + 1))), removed)
+#         else:
+#             print_space(int(number_of_elements / (2 ** counter)), removed)
+#         if removed is None:
+#             temp.append(None)
+#             temp.append(None)
+#         else:
+#             temp.append(removed.left)
+#             temp.append(removed.right)
+#         if len(tree_level) == 0:
+#             print("\n")
+#             tree_level = temp
+#             temp = []
+#             counter += 1
+#
+#
+# """ An algorithm/method to re-arrange the order of a given sequence of integers so that when the
+# data items are inserted sequentially into an initially empty BST, the newly created BST will be
+# a balanced BST.
+# """
+# def rearrange_sequence_for_bst_insertion(input_sequence):
+#
+#     return
+#
+#
+# """ A method to insert a given sequence of integers into a Binary Search Tree data structure. This
+# method does not perform any sorting before insertion, simply inserting elements of the sequence
+# in the order provided.
+# """
+# def create_balanced_bst(input_sequence):
+#     return
+#
+#
+# """ A method to demonstrate the tree shape of a Binary Search Tree data structure """
+# def print_bst_shape(input_bst):
+#     return
+#
+#
+# def main():
+#     sequence = [9, -1, 45, 6, 8, 21, 34, 5, 55, 65, 543, 18, 90, 122, 132, 0, 66, 100, -12, 17]
+#
+#     # print(sequence)
+#     # sequence.sort()
+#     # print(sequence)
+#
+#     # r = TreeNode(50)
+#     # for x in sequence:
+#     #     insert_into_bst(r, x)
+#     #
+#     # inorder(r)
+#
+#     root = TreeNode(1)
+#     temp = TreeNode(2)
+#     root.left = temp
+#     temp = TreeNode(3)
+#     root.right = temp
+#
+#     temp = TreeNode(4)
+#     root.left.left = temp
+#     temp = TreeNode(5)
+#     root.left.right = temp
+#     temp = TreeNode(6)
+#     root.right.left = temp
+#     temp = TreeNode(7)
+#     root.right.right = temp
+#
+#     temp = TreeNode(8)
+#     root.left.left.left = temp
+#     temp = TreeNode(9)
+#     root.left.left.right = temp
+#     temp = TreeNode(10)
+#     root.left.right.left = temp
+#     temp = TreeNode(11)
+#     root.left.right.right = temp
+#     temp = TreeNode(12)
+#     root.right.left.left = temp
+#     temp = TreeNode(13)
+#     root.right.left.right = temp
+#     temp = TreeNode(14)
+#     root.right.right.left = temp
+#     temp = TreeNode(15)
+#     root.right.right.right = temp
+#
+#     print_binary_tree(root)
+#
+#
+# main()
+
+class Treenode:
+    def __init__(self, data):
+        self.data = data
         self.left = None
         self.right = None
 
 
-# Source: CSP2348_M6_Binary Trees.pptx
-class BinaryTree:
+class Tree:
     def __init__(self):
         self.root = None
-        self.size = 0
 
 
-# https://www.geeksforgeeks.org/binary-search-tree-set-1-search-and-insertion/
-def insert_into_bst(root, e):
-    if root is None:
-        return TreeNode(e)
-    else:
-        if root.element is e:
-            return root
-        elif root.element < e:
-            root.right = insert_into_bst(root.right, e)
-        else:
-            root.left = insert_into_bst(root.left, e)
-    return root
-
-
-# https://www.geeksforgeeks.org/binary-search-tree-set-1-search-and-insertion/
-def inorder(root):
-    if root:
-        inorder(root.left)
-        print(root.element)
-        inorder(root.right)
-
-
-def print_space(n, removed):
-    for i in range(n):
-        print("\t", end="")
-    if removed is None:
-        print(" ", end="")
-    else:
-        print(removed.element, end="")
-
-
-def height_of_tree(root):
+def height(root):
     if root is None:
         return 0
-    return 1 + max(height_of_tree(root.left), height_of_tree(root.right))
+    return max(height(root.left), height(root.right)) + 1
 
 
-def print_binary_tree(root):
-    tree_level = []
-    temp = []
-    tree_level.append(root)
-    counter = 0
-    height = height_of_tree(root) - 1
-    number_of_elements = 2 ** (height + 1) - 1
-    while counter <= height:
-        removed = tree_level.pop(0)
-        if len(temp) == 0:
-            print_space(int(number_of_elements / (2 ** (counter + 1))), removed)
-        else:
-            print_space(int(number_of_elements / (2 ** counter)), removed)
-        if removed is None:
-            temp.append(None)
-            temp.append(None)
-        else:
-            temp.append(removed.left)
-            temp.append(removed.right)
-        if len(tree_level) == 0:
-            print("\n")
-            tree_level = temp
-            temp = []
-            counter += 1
+def getcol(h):
+    if h == 1:
+        return 1
+    return getcol(h - 1) + getcol(h - 1) + 1
 
 
-""" An algorithm/method to re-arrange the order of a given sequence of integers so that when the
-data items are inserted sequentially into an initially empty BST, the newly created BST will be
-a balanced BST.
-"""
-def rearrange_sequence_for_bst_insertion(input_sequence):
-        
-    return
+def printTree(M, root, col, row, height):
+    if root is None:
+        return
+    M[row][col] = root.data
+    printTree(M, root.left, col - pow(2, height - 2), row + 1, height - 1)
+    printTree(M, root.right, col + pow(2, height - 2), row + 1, height - 1)
 
 
-""" A method to insert a given sequence of integers into a Binary Search Tree data structure. This
-method does not perform any sorting before insertion, simply inserting elements of the sequence
-in the order provided.
-"""
-def create_balanced_bst(input_sequence):
-    return
+def TreePrinter():
+    h = height(myTree.root)
+    col = getcol(h)
+    M = [[0 for _ in range(col)] for __ in range(h)]
+    printTree(M, myTree.root, col // 2, 0, h)
+    for i in M:
+        for j in i:
+            if j == 0:
+                print(" ", end=" ")
+            else:
+                print(j, end=" ")
+        print("")
 
 
-""" A method to demonstrate the tree shape of a Binary Search Tree data structure """
-def print_bst_shape(input_bst):
-    return
-
-
-def main():
-    sequence = [9, -1, 45, 6, 8, 21, 34, 5, 55, 65, 543, 18, 90, 122, 132, 0, 66, 100, -12, 17]
-    
-    # print(sequence)
-    # sequence.sort()
-    # print(sequence)
-
-    # r = TreeNode(50)
-    # for x in sequence:
-    #     insert_into_bst(r, x)
-    #
-    # inorder(r)
-
-    root = TreeNode(1)
-    temp = TreeNode(2)
-    root.left = temp
-    temp = TreeNode(3)
-    root.right = temp
-
-    temp = TreeNode(4)
-    root.left.left = temp
-    temp = TreeNode(5)
-    root.left.right = temp
-    temp = TreeNode(6)
-    root.right.left = temp
-    temp = TreeNode(7)
-    root.right.right = temp
-
-    temp = TreeNode(8)
-    root.left.left.left = temp
-    temp = TreeNode(9)
-    root.left.left.right = temp
-    temp = TreeNode(10)
-    root.left.right.left = temp
-    temp = TreeNode(11)
-    root.left.right.right = temp
-    temp = TreeNode(12)
-    root.right.left.left = temp
-    temp = TreeNode(13)
-    root.right.left.right = temp
-    temp = TreeNode(14)
-    root.right.right.left = temp
-    temp = TreeNode(15)
-    root.right.right.right = temp
-
-    print_binary_tree(root)
-
-
-main()
+myTree = Tree()
+myTree.root = Treenode(1)
+myTree.root.left = Treenode(2)
+myTree.root.right = Treenode(3)
+myTree.root.left.left = Treenode(4)
+myTree.root.left.right = Treenode(5)
+myTree.root.right.left = Treenode(6)
+myTree.root.right.right = Treenode(7)
+TreePrinter()
